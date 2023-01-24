424206de04aa3a3d7c6a26cd57353027798b5e95
asmi046 <asmi046@gmail.com>
2023-01-24 20:21:23 +0300

    Растеневодство

--- resources/views/work.blade.php
+++ resources/views/work.blade.php
@@ -15,6 +15,47 @@
         <div class="_container">
 
             <x-breadcrumbs :title="$title"></x-breadcrumbs>
+
+            <div class="works_wrapper">
+
+                <div class="work_block">
+                    <div class="work_text">
+                        <h2>Растеневодство</h2>
+                        <p>Земельный банк компании насчитывает порядка 27 000 гектаров и расположен единым массивом  на территории трех районов Курской области. Мы выращиваем: озимую и яровую пшеницу овес, ячмень, подсолнечник, кукурузу, сою.</p>
+                        <a href="" class="btn">Подробнее</a>
+                    </div>
+
+                    <div class="work_img">
+                        <img src="{{asset('img/directions/01.jpg')}}" alt="">
+                    </div>
+                </div>
+
+                <div class="work_block">
+                    <div class="work_text">
+                        <h2>Животноводство</h2>
+                        <p>Полный цикл производства от выращивания кормовой базы до поставок готовой продукции. Наша компания располагает стадом  3 788  голов крупного рогатого скота фуражных и мясных пород</p>
+                        <a href="" class="btn">Подробнее</a>
+                    </div>
+
+                    <div class="work_img">
+                        <img src="{{asset('img/works/works_cow.jpg')}}" alt="">
+                    </div>
+                </div>
+
+                <div class="work_block">
+                    <div class="work_text">
+                        <h2>Конезавод</h2>
+                        <p>«Александровский конный завод №12», старейшее предприятие Курской области. Ведет свою историю с начала XIX века. Предприятие разводит лошадей пород русская рысистая и шетлендский пони.</p>
+                        <a href="" class="btn">Подробнее</a>
+                    </div>
+
+                    <div class="work_img">
+                        <img src="{{asset('img/works/work_hors.jpg')}}" alt="">
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     </section>
 
