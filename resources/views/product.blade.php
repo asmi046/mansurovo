2d5d77b58dbcf6d205fcd8b625e5cf961e6ece81
asmi046 <asmi046@gmail.com>
2023-01-30 14:11:57 +0300

    Разметка страницы продукции

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -16,6 +16,44 @@
         <div class="_container">
 
             <x-breadcrumbs :title="$title"></x-breadcrumbs>
+            <div class="products_in_page">
+                <div class="product">
+                    <img src="{{}}" alt="">
+                    <div class="text">
+                        <h2>Молоко</h2>
+                        <p>Предлагаем лошадей и жеребят от Александровского конезавода</p>
+                        <a href="#" class="btn">Подробнее</a>
+                    </div>
+                </div>
+
+                <div class="product">
+                    <img src="{{}}" alt="">
+                    <div class="text">
+                        <h2>Лошади</h2>
+                        <p>Предлагаем лошадей и жеребят от Александровского конезавода</p>
+                        <a href="#" class="btn">Подробнее</a>
+                    </div>
+                </div>
+
+                <div class="product">
+                    <img src="{{}}" alt="">
+                    <div class="text">
+                        <h2>Зерно</h2>
+                        <p>Зерновой материал для дальнейшей переработки</p>
+                        <a href="#" class="btn">Подробнее</a>
+                    </div>
+                </div>
+
+                <div class="product">
+                    <img src="{{}}" alt="">
+                    <div class="text">
+                        <h2>Мед</h2>
+                        <p>Предлагаем мед и продукты пчеловодства с собственных пасек</p>
+                        <a href="#" class="btn">Подробнее</a>
+                    </div>
+                </div>
+
+            </div>
         </div>
     </section>
 
