ef947f560e41afc6a330e5cf431678d01572f566
asmi046 <asmi046@gmail.com>
2023-03-02 19:02:47 +0300

    +

--- resources/views/layouts/all.blade.php
+++ resources/views/layouts/all.blade.php
@@ -9,7 +9,7 @@
 
         <meta property="og:locale" content="ru_RU" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="@yield('title') />
+        <meta property="og:title" content="@yield('title')" />
         <meta property="og:description" content="@yield('description')" />
         <meta property="og:url" content="{{route('home')}}" />
         <meta property="og:site_name" content="Агрохолдинг Мансурово" />
