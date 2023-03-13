0367d12f127b66f2a4c871a3d4aab24a2ad4ca4b
asmi046 <asmi046@gmail.com>
2023-03-13 10:45:56 +0300

    Меню

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -6,11 +6,12 @@
             <a href="index.html" class="logo-icon footer__logo"></a>
 
             <ul class="footer__menu">
-                <li class="footer__menu-item"><a href="#" class="footer__menu-item-link">О компании</a></li>
+                <x-menu-puncts></x-menu-puncts>
+                {{-- <li class="footer__menu-item"><a href="#" class="footer__menu-item-link">О компании</a></li>
                 <li class="footer__menu-item"><a href="#" class="footer__menu-item-link">Направления деятельности</a></li>
                 <li class="footer__menu-item"><a href="#" class="footer__menu-item-link">Продукция</a></li>
                 <li class="footer__menu-item"><a href="#" class="footer__menu-item-link">Вакансии</a></li>
-                <li class="footer__menu-item"><a href="#" class="footer__menu-item-link">Контакты</a></li>
+                <li class="footer__menu-item"><a href="#" class="footer__menu-item-link">Контакты</a></li> --}}
             </ul>
 
         </div>
