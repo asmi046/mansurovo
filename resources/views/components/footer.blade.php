8cad07609050af043fb2aafd7da41bbddee1bd14
asmi046 <asmi046@gmail.com>
2023-05-02 10:10:57 +0300

    В меню

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -9,10 +9,8 @@
                 <x-menu-puncts></x-menu-puncts>
             </ul>
 
-            <div class="social">
-                <a href="https://vk.com/club174172169" title="Агрокомплекс Мансурово в Vk" class="social_icon vk"></a>
-                <a href="https://t.me/mansurovogroup"  title="Агрокомплекс Мансурово в Telegram" class="social_icon tg"></a>
-            </div>
+            <x-social></x-social>
+
         </div>
 
     </div>
