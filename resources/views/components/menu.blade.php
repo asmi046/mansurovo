18160775ee377a85cce65813c00c4db10c96cb1f
asmi046 <asmi046@gmail.com>
2023-05-02 10:11:03 +0300

    ++

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,7 +1,11 @@
 <div class="menu_wrapper">
     <div class="menu_body">
+        <x-social></x-social>
+
         <ul class="puncts">
             <x-menu-puncts></x-menu-puncts>
         </ul>
+
+
     </div>
 </div>
---
+++ resources/views/components/social.blade.php
@@ -0,0 +1,4 @@
+<div class="social">
+    <a href="https://vk.com/club174172169" title="Агрокомплекс Мансурово в Vk" class="social_icon vk"></a>
+    <a href="https://t.me/mansurovogroup"  title="Агрокомплекс Мансурово в Telegram" class="social_icon tg"></a>
+</div>
